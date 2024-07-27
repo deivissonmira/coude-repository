b6d0bd916059c9dc30e51a4fb00efcb41b5571b5
deivissonmira <deivissonmira@gmail.com>
2024-07-27 20:04:01 -0300

    Update banco.sql
    Signed-off-by: deivissonmira <deivissonmira@gmail.com>

--- banco.sql
+++ banco.sql
@@ -2,7 +2,7 @@ Create database dores;
 use dores;
 
 CREATE TABLE IF NOT EXISTS `usuarios` (
-  `id` Char(02) primary key auto increment,
+  `id` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
   `empresa` varchar(50) NOT NULL UNIQUE,
   `seguimento` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
